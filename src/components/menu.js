faa2378bcf47ddbce47de810d585959fb1af5e20
KanishkaReddy0801 <krishnakanishkareddyalla@gmail.com>
2023-05-26 22:41:06 +0530

    navbar and menu

---
+++ src/components/menu.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import { BiX } from "react-icons/bi";
+import { BiMenu } from "react-icons/bi";
+import logo from "../assets/logo.svg"
+import "../App.css"
+
+export default function Menu () {
+    const [show, setShow] = useState(false)
+
+    function showMenu() {
+        setShow(true)
+    }
+    function closeMenu() {
+        setShow(false)
+    }
+    return (
+        <>
+        <header>
+            <img src={logo} alt="Logo" className="logo"></img>
+            <div className="menuBar">
+            <h3>Menu <BiMenu className="open" onClick={showMenu}></BiMenu></h3>
+            {
+            show? <div className="menu">
+               <BiX className="close" onClick={closeMenu}></BiX>
+               <ol>Home</ol>
+               <ol>Our Company</ol>
+               <ol>Our Leadership</ol>
+               <ol>Our Execution</ol>
+               <ol>Local Expertise</ol>
+               <ol>Work With Us</ol>
+               <ol>Resource Centre</ol>
+               <ol>Contact Us</ol>
+            </div> : null
+        }
+            </div>
+        </header>
+        </>
+    )
+}
