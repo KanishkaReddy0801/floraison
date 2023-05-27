8cc641da76538cff86db7a4d9cdbd225d57d5ad8
Guarav p <guaravp@MacBook-Air-2.local>
2023-05-27 14:28:43 +0530

    add banner and nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Banner from './components/banner';
 import Menu from './components/menu';
 
 function App() {
   return (
     <div className="App">
-      <Menu></Menu>
+      <Menu/>
+      <Banner/>
+      <h1>
+        Your Gateway to India
+      </h1>
+      <p>
+        Expand your global business and enter the Indian markets NOW!
+      </p>
     </div>
   );
 }
